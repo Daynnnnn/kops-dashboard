39143dbeb1835301fd4a948e2d267fd4cb57e405
Personal <personal@Dans-iMac.local>
2020-05-02 17:03:05 +0100

    Add KopsController

---
+++ app/Http/Controllers/KopsCommands.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use mikehaertl\shellcommand\Command as ShellCommand;
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+
+class KopsCommands extends Controller
+{
+
+	private function SpawnBucket($bucket) {
+		$s3Client = new S3Client([
+		    'profile' => 'default',
+		    'region' => 'eu-west-1',
+		    'version' => '2006-03-01'
+		]);
+	
+		$OwnedBuckets = $s3Client->listBuckets();
+		foreach ($OwnedBuckets['Buckets'] as $OwnedBucket) {
+		    if($OwnedBucket['Name'] === $bucket) {
+		    	return true;
+		    }
+		}
+ 		
+    	try {
+    	    $s3Client->createBucket([
+    	        'Bucket' => $bucket,
+    	    ]);
+    	    return true;
+    	} catch (AwsException $e) {
+    	    return false;
+    	}
+
+	}
+
+    public function create(Request $request) {
+
+    	$bucket = $request->input('name').'-'.substr(md5($request->input('name')), 0, 6);
+
+    	if ($this->SpawnBucket($bucket) !== true) {
+    		return response('Couldn\'t create bucket ' . $bucket, 200)->header('Content-Type', 'text/plain');
+    	}
+
+    	$command = new ShellCommand('kops create cluster');
+    	$command->addArg('--name='.$request->input('name'));
+    	$command->addArg('--state=s3://'.$bucket);
+    	$command->addArg('--zones='.$request->input('zones'));
+    	$command->addArg('--node-count='.$request->input('node_count'));
+    	if ($request->input('confirm') === 'true') {
+    		$command->addArg('--yes');
+    	}
+
+    	if ($command->execute()) {
+		    return response($command->getOutput(), 200)->header('Content-Type', 'text/plain');
+		} else {
+		    return response($command->getError(), 500)->header('Content-Type', 'text/plain');
+		}
+    }
+
+    public function delete(Request $request) {
+
+		$bucket = $request->input('name').'-'.substr(md5($request->input('name')), 0, 6);
+
+    	$command = new ShellCommand('kops delete cluster');
+    	$command->addArg('--name='.$request->input('name'));
+    	$command->addArg('--state=s3://'.$bucket);
+    	if ($request->input('confirm') === 'true') {
+    		$command->addArg('--yes');
+    	}
+
+    	if ($command->execute()) {
+		    return response($command->getOutput(), 200)->header('Content-Type', 'text/plain');
+		} else {
+		    return response($command->getError(), 500)->header('Content-Type', 'text/plain');
+		}
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/kops/create', 'KopsCommands@create');
+Route::post('/kops/delete', 'KopsCommands@delete');
