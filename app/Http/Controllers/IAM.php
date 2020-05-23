08a3d58731458acd560226713d7d7877771045ab
Dan Pegg <daniel.pegg7849@gmail.com>
2020-05-23 10:58:29 +0100

    Remove decrypt for IAM controller, add decrypt to kops controller

--- app/Http/Controllers/IAM.php
+++ app/Http/Controllers/IAM.php
@@ -10,16 +10,12 @@ class IAM extends Controller
 	/*
 		Credits to https://wppeople.net/blog/a-simple-two-way-function-to-encrypt-or-decrypt-a-string/ for encrypt function
 	*/
-	private function Encryption($string, $password, $action = 'e') {
+	private function Encrypt($string, $password) {
 
-		if ($action === 'e' && strlen(base64_encode($string)) >= 16) {
+		if (strlen(base64_encode($string)) >= 16) {
 			$SplitStringArray = str_split(base64_encode($string), 16);
-		} elseif($action === 'e') {
+		} else {
 			$SplitStringArray['0'] = base64_encode($string);
-		}
-
-		if ($action === 'd') {
-			$SplitStringArray = explode(',', $string);
 		}
 
 		foreach ($SplitStringArray as $increment => $SplitString) {
@@ -28,18 +24,10 @@ class IAM extends Controller
 			$key = hash( 'sha256', env('ENCRYPT_KEY') );
 			$iv = substr( hash('sha256', $password), 0, 16 );
 
-			if ( $action == 'e' ) {
-				$HandledSplitStringArray[$increment] = openssl_encrypt( $SplitString, env('ENCRYPT_METHOD'), $key, 0, $iv );
-			} elseif( $action == 'd' ) {
-				$HandledSplitStringArray[$increment] = openssl_decrypt( $SplitString, env('ENCRYPT_METHOD'), $key, 0, $iv );
-			}
+			$HandledSplitStringArray[$increment] = openssl_encrypt( $SplitString, env('ENCRYPT_METHOD'), $key, 0, $iv );
 		}
 
-		if ( $action == 'e' ) {
-			return implode(',', $HandledSplitStringArray);
-		} elseif( $action == 'd' ) {
-			return base64_decode (implode('', $HandledSplitStringArray));
-		}
+		return implode(',', $HandledSplitStringArray);
 	}
 
 	public function create(Request $request) {
@@ -80,29 +68,5 @@ class IAM extends Controller
 		DB::table('iam')->where('id', '=', $Record)->delete();
 
 		return response('Deleted IAM Keys', 200)->header('Content-Type', 'text/plain');
-	}
-
-	public function get(Request $request) {
-
-		// [KD-TMP] needs some sort of auth
-		$EncryptedIAM = false;
-
-		$iamTable = DB::table('iam')->get();
-
-		foreach ($iamTable as $key => $iamRecord) {
-			if ($iamRecord->name === $request->input('IAM_NAME')) {
-				$EncryptedIAM = true;
-				$EncryptedIAMKey = $iamRecord->encrypted_iam_key;
-				$EncryptedIAMSecret = $iamRecord->encrypted_iam_secret;
-			}
-		}
-
-		if ($EncryptedIAM !== true) {
-			return response('Record with that name not in database', 400)->header('Content-Type', 'text/plain');
-		}
-
-		$UnencryptedIAM = $this->Encryption($EncryptedIAMKey, $request->input('IAM_PASSWORD'), 'd') . ":" . $this->Encryption($EncryptedIAMSecret, $request->input('IAM_PASSWORD'), 'd');
-
-		return response($UnencryptedIAM, 200)->header('Content-Type', 'text/plain');
 	}
 }
--- app/Http/Controllers/KopsCommands.php
+++ app/Http/Controllers/KopsCommands.php
@@ -10,45 +10,28 @@ use Aws\Exception\AwsException;
 class KopsCommands extends Controller
 {
 
-	private function SpawnBucket($bucket) {
-		$s3Client = new S3Client([
-			'profile' => 'default',
-			'region' => 'eu-west-1',
-			'version' => '2006-03-01'
-		]);
-	
-		$OwnedBuckets = $s3Client->listBuckets();
-		foreach ($OwnedBuckets['Buckets'] as $OwnedBucket) {
-			if($OwnedBucket['Name'] === $bucket) {
-				return true;
-			}
-		}
- 		
-		try {
-			$s3Client->createBucket([
-				'Bucket' => $bucket,
-			]);
-			return true;
-		} catch (AwsException $e) {
-			return false;
+	private function Decryption($string, $password) {
+    	$SplitStringArray = explode(',', $string);
+
+    	foreach ($SplitStringArray as $increment => $SplitString) {
+			$HandledSplitStringArray[$increment] = false;
+
+			$key = hash( 'sha256', env('ENCRYPT_KEY') );
+			$iv = substr( hash('sha256', $password), 0, 16 );
+
+			$HandledSplitStringArray[$increment] = openssl_decrypt( $SplitString, env('ENCRYPT_METHOD'), $key, 0, $iv );
 		}
 
-	}
+		return base64_decode (implode('', $HandledSplitStringArray));
+    }
 
-	public function create(Request $request) {
 
-		$bucket = $request->input('name').'-'.substr(md5($request->input('name')), 0, 6);
+	public function getInstanceGroups(Request $request) {
 
-		if ($this->SpawnBucket($bucket) !== true) {
-			return response('Couldn\'t create bucket ' . $bucket, 200)->header('Content-Type', 'text/plain');
-		}
-
-		$command = new ShellCommand('kops create cluster');
-		$command->addArg('--name='.$request->input('name'));
-		$command->addArg('--state=s3://'.$bucket);
-		$command->addArg('--zones='.$request->input('zones'));
-		$command->addArg('--node-count='.$request->input('node_count'));
-		if ($request->input('confirm') === 'true') {
+		$command = new ShellCommand('export && export && kops get ig');
+		$command->addArg('--name='.$request->input('KOPS_NAME'));
+		$command->addArg('--state=s3://'.$request->input('KOPS_STATE_STORE'));
+		if ($request->input('KOPS_CONFIRM') === 'true') {
 			$command->addArg('--yes');
 		}
 
@@ -57,24 +40,6 @@ class KopsCommands extends Controller
 		} else {
 			return response($command->getError(), 500)->header('Content-Type', 'text/plain');
 		}
-	}
-
-	public function delete(Request $request) {
-
-		$bucket = $request->input('name').'-'.substr(md5($request->input('name')), 0, 6);
-
-		$command = new ShellCommand('kops delete cluster');
-		$command->addArg('--name='.$request->input('name'));
-		$command->addArg('--state=s3://'.$bucket);
-		if ($request->input('confirm') === 'true') {
-			$command->addArg('--yes');
-		}
-
-		if ($command->execute()) {
-			return response($command->getOutput(), 200)->header('Content-Type', 'text/plain');
-		} else {
-			return response($command->getError(), 500)->header('Content-Type', 'text/plain');
-		}
-	}
+	}	
 
 }
